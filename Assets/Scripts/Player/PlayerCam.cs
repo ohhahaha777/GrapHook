4a544ea0f5ed874f1f71ec76b393fcf3a12d8365
ohhahaha777 <913971053@qq.com>
2024-05-15 23:07:36 +0800

    更新

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -1,35 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCam : MonoBehaviour
+namespace Player
 {
-    public float sensitivityX;
-    public float sensitivityY;
-    
-    public Transform orientation;
-
-    private float _rotationX;
-    private float _rotationY;
-
-    private void Start()
+    public class PlayerCam : MonoBehaviour
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    private void Update()
-    {
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
-
-        _rotationX -= mouseY;
-        _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
-        _rotationY += mouseX;
+        public float sensitivityX;
+        public float sensitivityY;
         
-        transform.rotation = Quaternion.Euler(_rotationX, _rotationY,0);
-        orientation.rotation = Quaternion.Euler(0,_rotationY,0);
+        private float _rotationX;
+        private float _rotationY;
+
+        public Transform followPos;
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+
+            _rotationX -= mouseY;
+            _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
+            _rotationY += mouseX;
         
+            transform.rotation = Quaternion.Euler(_rotationX, _rotationY,0);
+            transform.position = followPos.position;
+        }
     }
 }
